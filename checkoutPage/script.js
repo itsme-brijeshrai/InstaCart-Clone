b4c569c2335a63872ab2aa941e6a19a92036a02f
Brijesh Rai <Brijeshrai9820@gmail.com>
2022-04-02 09:40:33 +0530

    updated homePage

--- checkoutPage/script.js
+++ checkoutPage/script.js
@@ -1,4 +1,5 @@
 document.querySelector(".submit").addEventListener("click",submitinfo);
+    event.preventDefault();
     function submitinfo(){
         console.log("submitted");
         window.location.href="payment.html"
